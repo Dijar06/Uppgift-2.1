3ec6074093cd3a82db6c04e0aa73618220386d71
Dijar06 <dize06001@utb.vaxjo.se>
2023-09-15 12:25:54 +0200

    Add project files.

---
+++ Uppgift 2.1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Uppgift2._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Vad är din favoritbok?");
+            string x = Console.ReadLine();
+            Console.WriteLine(x + " är en bra bok.");
+        }
+    }
+}
